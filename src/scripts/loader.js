4beb980aa5153a65b1652f5ca3fdfd212e341778
Marcus Carter <marcoose408@gmail.com>
2026-05-10 14:23:33 -0400

    navbar / footer

---
+++ src/scripts/loader.js
@@ -0,0 +1,35 @@
+
+// async function loadComponent(selector, path) {
+//     const res = await fetch(path);
+//     const html = await res.text();
+//     document.querySelector(selector).innerHTML = html;
+// }
+
+// loadComponent('#footer', '/src/components/footer.html');
+// loadComponent('#navbar', '/src/components/navbar.html');
+
+async function loadComponent(selector, path) {
+    const res = await fetch(path);
+    const html = await res.text();
+    document.querySelector(selector).innerHTML = html;
+}
+
+async function init() {
+    await loadComponent('#navbar', '/src/components/navbar.html');
+    await loadComponent('#footer', '/src/components/footer.html');
+
+    let lastScroll = 0;
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.scrollY;
+        if (currentScroll > lastScroll && currentScroll > 100) {
+            navbar.classList.add('hidden');
+        } else {
+            navbar.classList.remove('hidden');
+        }
+        lastScroll = currentScroll;
+    });
+}
+
+init();
